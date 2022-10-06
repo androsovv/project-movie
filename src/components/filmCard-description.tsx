b22374d7338f33b9654556e0cddf861fe69e7287
Vladimir workPC <advogad1@gmail.com>
2022-10-06 12:01:02 +0300

    page about added

--- src/components/filmCard-description.tsx
+++ src/components/filmCard-description.tsx
@@ -21,25 +21,43 @@ const FilmCardDescription = () => {
         vote_average,
         genre_ids,
         id,
-        poster_path
+        poster_path,
+        original_title
     } = getFilm(Number(filmId));
 
     console.log(getFilm(Number(filmId)));
 
     return (
         <div>
-            <div className="mainInfo" style={{
-                background: `url(https://image.tmdb.org/t/p/w500${backdrop_path}) no-repeat left / 200%`}}>
-                <div className="leftSide" style={{
-                    background: `url(https://image.tmdb.org/t/p/w500${poster_path}) no-repeat center/contain `}}>
+            <div className="filmInfo">
+                <div className="mainInfo" style={{
+                    background: `url(https://image.tmdb.org/t/p/w500${backdrop_path}) no-repeat left / 200%`}}>
+                    <div className="leftSide" style={{
+                        background: `url(https://image.tmdb.org/t/p/w500${poster_path}) no-repeat center/contain `}}>
+                    </div>
+                    <div className="rightSide">
+                        <div className="title">{title}</div>
+                        <div className="raiting">{"Рейтинг: " + vote_average}</div>
+                        <div className="overview">{overview}</div>
+                    </div>
                 </div>
-                <div className="rightSide">
-                    <div className="title">{title}</div>
-                    <div className="raiting">{"Рейтинг: " + vote_average}</div>
-                    <div className="overview">{overview}</div>
+                <div className="filmDetails">
+                    <ul className='filmDetails__left'>
+                        <li>Статус</li>
+                        <li>Дата выхода</li>
+                        <li>Язык оригинала</li>
+                        <li>Оригинальное название</li>
+                        <li>Рейтинг</li>
+                    </ul>
+                    <ul className='filmDetails__right'>
+                        <li>Вышел</li>
+                        <li>{release_date}</li>
+                        <li>{original_language}</li>
+                        <li>{original_title}</li>
+                        <li>{vote_average}</li>
+                    </ul>
                 </div>
             </div>
-
         </div>
     );
 };
